88bbeaf3832dcab847b8577673dbc860b06992b8
Igor Melo <igorjmelo4@gmail.com>
2018-08-06 18:40:44 -0300

    new commit

---
+++ 04_nba_app/src/hoc/Layout/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+    
+    state = {
+
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Layout;
--- 04_nba_app/src/routes.js
+++ 04_nba_app/src/routes.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom'
 
 import Home from './components/Home/home'
+import Layout from './hoc/Layout/layout'
 
 class Routes extends Component {
     render() {
         return (
-
-            <Switch>
-                <Route path="/" exact component={Home}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                </Switch>
+            </Layout>
         )
     }
 }
