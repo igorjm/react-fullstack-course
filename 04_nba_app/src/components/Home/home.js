3ad26bf82ed454d28199effaef225bdc7c3343bc
Igor Melo <igorjmelo4@gmail.com>
2018-08-20 23:15:54 -0300

    adding header w/ sidebar and slider with articles

--- 04_nba_app/src/components/Home/home.js
+++ 04_nba_app/src/components/Home/home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import NewsSlider from '../widgets/NewsSlider/slider'
+
 const Home = () => {
     return (
         <div>
-            Home
+            <NewsSlider
+                type="featured"
+                start={0}
+                amount={3}
+                settings={{
+                    dots: false,
+                }}
+            />
         </div>
     )
 }
--- 04_nba_app/src/hoc/Layout/layout.js
+++ 04_nba_app/src/hoc/Layout/layout.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import './layout.css'
+
+import Header from '../../components/Header/header'
+import Footer from '../../components/Footer/footer'
 
 class Layout extends Component {
     
     state = {
+        showNav: false
+    }
 
+    toggleSiveNav = (action) => {
+        this.setState=({
+            showNav:action
+        })
     }
     
     render() {
         return (
             <div>
-                
+                <Header
+                    showNav={this.state.showNav}
+                    onHideNav={() => this.toggleSiveNav(false)}
+                    onOpenNav={() => this.toggleSiveNav(true)}
+                />
+                {this.props.children}
+                <Footer/>
             </div>
         );
     }
