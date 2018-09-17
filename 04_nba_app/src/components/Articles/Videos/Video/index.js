859616fc2ac2dae1ddedfa4e41afd4e390f7d075
Igor Melo <igorjmelo4@gmail.com>
2018-09-17 20:28:52 -0300

    finishing videos and videos related

--- 04_nba_app/src/components/Articles/Videos/Video/index.js
+++ 04_nba_app/src/components/Articles/Videos/Video/index.js
@@ -4,12 +4,15 @@ import {URL} from '../../../../config'
 
 import styles from '../../articles.css'
 import Header from './header'
+import VideosRelated from '../../../widgets/VideosList/VideosRelated/videosRelated';
 
 class VideoArticle extends Component {
 
     state = {
         article: [],
-        team: []
+        team: [],
+        teams: [],
+        related: []
     }
 
     componentWillMount() {
@@ -23,6 +26,23 @@ class VideoArticle extends Component {
                     article,
                     team: response.data
                 })
+
+                this.getRelated()
+            })
+        })
+    }
+
+    getRelated = () => {
+        axios.get(`${URL}/teams`)
+        .then( response => {
+            let teams = response.data
+
+            axios.get(`${URL}/videos?q=${this.state.team[0].city}&_limit=3`)
+            .then( response => {
+                this.setState({
+                    teams,
+                    related: response.data
+                })
             })
         })
     }
@@ -34,7 +54,19 @@ class VideoArticle extends Component {
         return (
             <div>
                 <Header teamData={team[0]}/>
-                
+                <div className={styles.videoWrapper}>
+                    <h1>{article.title}</h1>
+                    <iframe
+                        title="videoplayer"
+                        width="100%"
+                        height="300px"
+                        src={`https://www.youtube.com/embed/${article.url}`}
+                    ></iframe>
+                </div>
+                <VideosRelated
+                    data={this.state.related}
+                    teams={this.state.teams}
+                />
             </div>
         )
     }
