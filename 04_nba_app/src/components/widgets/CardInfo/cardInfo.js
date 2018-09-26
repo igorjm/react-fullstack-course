82fbe60b648321652e99c0b9b4fc26e6cb087586
Igor Melo <igorjmelo4@gmail.com>
2018-09-25 22:02:36 -0300

    finished the migation of project to firebase

--- 04_nba_app/src/components/widgets/CardInfo/cardInfo.js
+++ 04_nba_app/src/components/widgets/CardInfo/cardInfo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
+import moment from 'moment'
 
 import styles from './cardInfo.css'
 
@@ -7,11 +8,15 @@ const CardInfo = (props) => {
 
     const teamName = (teams, team) => {
         let data = teams.find((item) => {
-            return item.id === team
+            return item.teamId === team
         })
         if(data) {
             return data.name
         }
+    }
+
+    const formatDate = (date) => {
+        return moment(date).format(' MM-DD-YYYY')
     }
 
     return(
@@ -21,7 +26,7 @@ const CardInfo = (props) => {
             </span>
             <span className={styles.date}>
                 <FontAwesome name="clock-o"/>
-                {props.date}
+                {formatDate(props.date)}
             </span>
         </div>
     )
--- 04_nba_app/src/components/widgets/NewsList/newsList.js
+++ 04_nba_app/src/components/widgets/NewsList/newsList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import {Link} from 'react-router-dom'
-import axios from 'axios'
+import {firebaseTeams, firebaseArticles, firebaseLooper} from '../../../firebase'
 
-import {URL} from '../../../config'
 import styles from './newsList.css'
 
 import Button from '../Buttons/buttons'
@@ -25,27 +24,47 @@ class NewsList extends Component {
     
     request = (start, end) => {
         if(this.state.teams.length < 1) {
-            axios.get(`${URL}/teams`)
-            .then( response => {
+            firebaseTeams.once('value')
+            .then((snapshot) => {
+                const teams = firebaseLooper(snapshot)
                 this.setState({
-                    teams:response.data
+                    teams
                 })
             })
         }
-
-        axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
-        .then( response => {
+            // axios.get(`${URL}/teams`)
+            // .then( response => {
+            //     this.setState({
+            //         teams:response.data
+            //     })
+            // })
+        firebaseArticles.orderByChild('id').startAt(start).endAt(end).once('value')
+        .then((snapshot) => {
+            const articles = firebaseLooper(snapshot)
             this.setState({
-                items:[...this.state.items, ...response.data],
+                items:[...this.state.items, ...articles],
                 start,
                 end
             })
         })
+        .catch(e => {
+            console.log(e)
+        })
+
+        // axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
+        // .then( response => {
+        //     this.setState({
+        //         items:[...this.state.items, ...response.data],
+        //         start,
+        //         end
+        //     })
+        // })
+
     }
 
     loadMore = () => {
         let end = this.state.end + this.state.amount
-        this.request(this.state.end, end)
+        this.request(this.state.end + 1, end)
     }
 
     renderNews = (type) => {
