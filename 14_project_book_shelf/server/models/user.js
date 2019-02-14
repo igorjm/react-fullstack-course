cc1acc085b12f2a643fbcbdf56f7dab40e848209
Igor Melo <igorjmelo4@gmail.com>
2019-02-14 21:19:26 -0200

    adding user routes

--- 14_project_book_shelf/server/models/user.js
+++ 14_project_book_shelf/server/models/user.js
@@ -34,6 +34,67 @@ const userSchema = mongoose.Schema({
   }
 })
 
+userSchema.pre('save', function(next) {
+    var user = this
+    
+    if(user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if(err) return callback(err)
+
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback) {
+    var user = this
+    var token = jwt.sign(user._id.toHexString(), config.SECRET)
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return callback(err)
+
+        callback(null, user)
+    })
+}
+
+userSchema.methods.deleteToken = function(token, callback) {
+    var user = this
+
+    user.update({$unset: {token: 1}}, (err, user) => {
+        if(err) return callback(err)
+
+        callback(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, callback) {
+    var user = this
+
+    jwt.verify(token, config.SECRET, function(err, decode) {
+        user.findOne({"_id": decode, "token": token}, function(err, user) {
+            if(err) return callback(err)
+
+            callback(null, user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
--- 14_project_book_shelf/server/server.js
+++ 14_project_book_shelf/server/server.js
@@ -11,11 +11,20 @@ mongoose.connect(config.DATABASE)
 
 const {User} = require('./models/user')
 const {Book} = require('./models/book')
+const {auth} = require('./middleware/auth')
 
 app.use(bodyParser.json())
 app.use(cookieParser())
 
 // GET //
+app.get('/api/logout', auth, (req, res) => {
+    req.user.deleteToken(req.tokne, (err, user) => {
+        if(err) return res.status(400).send(err)
+        
+        res.sendStatus(200)
+    })
+})
+
 app.get('/api/get-book', (req, res) => {
     let id = req.query.id
 
@@ -40,6 +49,35 @@ app.get('/api/books', (req, res) => {
                })
 })
 
+app.get('/api/get-reviewer', (req, res) => {
+    let id = req.query.id
+
+    User.findById(id, (err, doc) => {
+        if(err) return res.status(400).send(err)
+
+        res.json({
+            name: doc.name,
+            lastname: doc.lastname
+        })
+    })
+})
+
+app.get('/api/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if(err) return res.status(400).send(err)
+
+        res.status(200).send(users)
+    })
+})
+
+app.get('/api/user-posts', (req, res) => {
+    Book.find({ownerId: req.query.user}).exec((err, docs) => {
+        if(err) return res.status(400).send(err)
+
+        res.send(docs)
+    })
+})
+
 // POST //
 app.post('/api/book', (req, res) => {
     const book = new Book(req.body)
@@ -50,6 +88,42 @@ app.post('/api/book', (req, res) => {
         res.status(200).json({
             post: true,
             bookId: doc._id
+        })
+    })
+})
+
+app.post('/api/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false})
+        
+        res.status(200).json({
+            success: true,
+            user: doc
+        })
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    User.findOne({'email': req.body.email}, (err, user) => {
+        if(!user) return res.json({isAuth: false, message: 'Auth failed. User not found'})
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({
+                isAuth: false,
+                message: 'Wrong password!'
+            })
+
+            user.generateToken((err, user) => {
+                if(err) return res.send(400).send(err)
+
+                res.cookie('auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                })
+            })
         })
     })
 })
