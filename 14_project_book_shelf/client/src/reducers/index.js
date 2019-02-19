e10140a715555fb62f03b970dbdb45ff7511b0ee
Igor Melo <igorjmelo4@gmail.com>
2019-02-18 22:54:29 -0300

    implementing header layout

--- 14_project_book_shelf/client/src/reducers/index.js
+++ 14_project_book_shelf/client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import books from './books-reducer'
+import user from './user-reducer'
 
 const rootReducer = combineReducers({
-
+    books, 
+    user
 })
 
 export default rootReducer
--- 14_project_book_shelf/client/src/routes.js
+++ 14_project_book_shelf/client/src/routes.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Home from './components/Home/home'
+import Layout from './hoc/layout';
+
 const Routes = () => {
     return (
-        <div>
-            
-        </div>
+        <Layout>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Layout>
     );
 };
 
